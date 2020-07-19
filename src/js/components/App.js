58d349d3b8563738d3d3a3159a1994526c6cc480
Jenny Belanger <jbelangr@gmail.com>
2020-07-19 17:44:30 -0400

    Prompt to create library file if there are no songs

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -1,8 +1,16 @@
 import Header from './Header';
+import ImportModal from './modals/ImportModal';
 import React from 'react';
+import { selectNumSongs } from '../appSlice';
 import Table from './Table';
+import { useSelector } from 'react-redux';
 
 export default function App() {
+	const numSongs = useSelector(selectNumSongs);
+	if (numSongs <= 0) {
+		return <ImportModal />;
+	}
+
 	return (
 		<main>
 			<Header />
