182fcb9234d5e13087a498c898aeec814fe0189c
Jenny Belanger <jbelangr@gmail.com>
2020-07-29 22:12:05 -0400

    Rename queue to app

--- src/js/observer.js
+++ src/js/observer.js
@@ -3,22 +3,22 @@ import store from './store';
 import watch from 'redux-watch';
 
 export default () => {
-	const watchSortColumn = watch(store.getState, 'queue.sortColumn');
+	const watchSortColumn = watch(store.getState, 'app.sortColumn');
 	store.subscribe(watchSortColumn((newVal) => {
 		Storage.set('sortColumn', newVal);
 	}));
 
-	const watchSortDirection = watch(store.getState, 'queue.sortDirection');
+	const watchSortDirection = watch(store.getState, 'app.sortDirection');
 	store.subscribe(watchSortDirection((newVal) => {
 		Storage.set('sortDirection', newVal);
 	}));
 
-	const watchRepeat = watch(store.getState, 'queue.repeat');
+	const watchRepeat = watch(store.getState, 'app.repeat');
 	store.subscribe(watchRepeat((newVal) => {
 		Storage.set('repeat', newVal);
 	}));
 
-	const watchShuffle = watch(store.getState, 'queue.shuffle');
+	const watchShuffle = watch(store.getState, 'app.shuffle');
 	store.subscribe(watchShuffle((newVal) => {
 		Storage.set('shuffle', newVal);
 	}));
