5630d7b774ad6157f97f3b487a17c1c32e61a9c0
Jenny Belanger <jbelangr@gmail.com>
2020-08-15 14:16:24 -0400

    Add volume control

--- src/js/components/NowPlaying.js
+++ src/js/components/NowPlaying.js
@@ -4,12 +4,14 @@ import { useDispatch, useSelector } from 'react-redux';
 import { prettyTime } from '../helpers/datetime';
 import Storage from '../helpers/Storage';
 import Range from './Range';
+import Volume from './Volume';
 
 export default function NowPlaying() {
 	const dispatch = useDispatch();
 	const song = useSelector(selectCurrentSong);
 	const isPlaying = useSelector(selectIsPlaying);
 	const [currentTime, setCurrentTime] = useState(0);
+	const [volume, setVolume] = useState(Storage.get('volume', 1.0));
 	const [isActuallyPlaying, setIsActuallyPlaying] = useState(false);
 	const [audio, setAudio] = useState(null);
 	if (!song) {
@@ -47,9 +49,11 @@ export default function NowPlaying() {
 				onTimeUpdate={onTimeUpdate}
 				ref={(element) => { setAudio(element); }}
 				src={`http://localhost:2000/?path=${encodeURIComponent(song.path)}`}
+				volume={volume}
 			/>
 			<div id="now-playing-title">{song.title}</div>
 			<div id="now-playing-artist">{song.artist}</div>
+			<Volume audio={audio} setVolume={setVolume} volume={volume} />
 			<div id="now-playing-time">
 				<div id="now-playing-time-current">{prettyTime(currentTime, song.duration)}</div>
 				<Range id="position" max={song.duration} onChange={onSeek} step={1} value={currentTime} />
---
+++ src/js/components/Volume.js
@@ -0,0 +1,29 @@
+import '../../scss/components/Volume.scss';
+import Range from './Range';
+import React from 'react';
+import Storage from '../helpers/Storage';
+import { ReactComponent as VolumeHighIcon } from '../../svg/volume-high.svg';
+import { ReactComponent as VolumeLowIcon } from '../../svg/volume-low.svg';
+import { ReactComponent as VolumeOffIcon } from '../../svg/volume-off.svg';
+
+export default function NowPlaying({ audio, setVolume, volume }) {
+	const onChangeVolume = (e) => {
+		const newVolume = parseFloat(e.target.value);
+		audio.volume = newVolume;
+		setVolume(newVolume);
+		Storage.set('volume', newVolume);
+	};
+	let VolumeIcon = VolumeHighIcon;
+	if (volume <= 0.0) {
+		VolumeIcon = VolumeOffIcon;
+	} else if (volume < 1.0) {
+		VolumeIcon = VolumeLowIcon;
+	}
+
+	return (
+		<div id="volume">
+			<VolumeIcon height="16" id="volume-icon" width="16" />
+			<Range id="volume-input" max="1" step="0.1" onChange={onChangeVolume} value={volume} />
+		</div>
+	);
+}
