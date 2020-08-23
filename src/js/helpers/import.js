a8fdaeb236024c6dae640845c30590f2c1a32259
Jenny Belanger <jbelangr@gmail.com>
2020-08-23 14:46:18 -0400

    Import additional values

--- src/js/helpers/import.js
+++ src/js/helpers/import.js
@@ -26,8 +26,8 @@ export default (file) => (
 		.then((xml) => {
 			const json = plist.parse(xml);
 			const output = {};
+			let id = 1;
 			Object.values(json.Tracks).forEach((song) => {
-				const id = getStringProperty(song, 'Track ID');
 				let path = getStringProperty(song, 'Location');
 				if (path) {
 					path = decodeURI(path.replace('file://', ''));
@@ -37,15 +37,28 @@ export default (file) => (
 					checked: !getBoolProperty(song, 'Disabled'),
 					title: getStringProperty(song, 'Name'),
 					artist: getStringProperty(song, 'Artist'),
+					album_artist: getStringProperty(song, 'Album Artist'),
 					album: getStringProperty(song, 'Album'),
 					year: getStringProperty(song, 'Year'),
+					track: getStringProperty(song, 'Track Number'),
+					track_count: getStringProperty(song, 'Track Count'),
+					disc: getStringProperty(song, 'Disc Number'),
+					disc_count: getStringProperty(song, 'Disc Count'),
 					genre: getStringProperty(song, 'Genre'),
 					rating: parseInt(getStringProperty(song, 'Rating'), 10) / 20,
+					composer: getStringProperty(song, 'Composer'),
+					grouping: getStringProperty(song, 'Grouping'),
 					duration: getStringProperty(song, 'Total Time'),
+					start_time: getStringProperty(song, 'Start Time'),
+					end_time: getStringProperty(song, 'Stop Time'),
+					num_plays: getStringProperty(song, 'Play Count'),
+					num_skips: getStringProperty(song, 'Skip Count'),
 					last_played: getDateProperty(song, 'Play Date UTC'),
+					last_skipped: getDateProperty(song, 'Skip Date'),
 					date_added: getDateProperty(song, 'Date Added'),
 					path,
 				};
+				id += 1;
 			});
 			return output;
 		})
