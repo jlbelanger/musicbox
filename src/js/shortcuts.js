0646356220d999c0bc4bfb072feac9d7ee4938e4
Jenny Belanger <jbelangr@gmail.com>
2020-08-23 14:31:57 -0400

    Jump to top/bottom of table

--- src/js/shortcuts.js
+++ src/js/shortcuts.js
@@ -21,10 +21,22 @@ function jumpToSong(e) {
 	let rows = window.musicboxTable.table.searchData(sort[0].column, 'starts', keySequence.join(''));
 	rows = sortRows(rows, sort);
 	if (rows.length > 0) {
-		window.musicboxTable.table.scrollToRow(rows[0].id, 'top', false);
+		window.musicboxTable.table.scrollToRow(rows[0].id, 'top',);
+	}
+}
+
+function homeEnd(e) {
+	if (e.key === 'Home') {
+		const rows = window.musicboxTable.table.rowManager.activeRows;
+		window.musicboxTable.table.scrollToRow(rows[0].data.id, 'top');
+	} else if (e.key === 'End') {
+		const rows = window.musicboxTable.table.rowManager.activeRows;
+		const num = window.musicboxTable.table.rowManager.activeRowsCount - 1;
+		window.musicboxTable.table.scrollToRow(rows[num].data.id, 'bottom');
 	}
 }
 
 export default () => {
 	window.addEventListener('keyup', jumpToSong, true);
+	window.addEventListener('keyup', homeEnd, true);
 };
